be658ddce0ec6eb85e798065c63a37dd73a67008
Emman-pip <parrenoemmanuel756@gmail.com>
2025-03-15 10:24:39 +0800

    Minor changes.
    Spacing and brackets.

--- main.c
+++ main.c
@@ -1,58 +1,54 @@
-#include <string.h>
 #include <ctype.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #define SIZE 1000
 
-
-
-char ***mapInput(char *filename, int *returnSize, int *questionsCount)
-{
+char ***mapInput(char *filename, int *returnSize, int *questionsCount) {
   FILE *file = fopen(filename, "r");
-  
+
   char *str = malloc(sizeof(char) * SIZE);
 
   char ***data = malloc(sizeof(char **) * 2);
-  data[0] = malloc(sizeof(char*) * SIZE);
-  data[1] = malloc(sizeof(char*) * SIZE);
+  data[0] = malloc(sizeof(char *) * SIZE);
+  data[1] = malloc(sizeof(char *) * SIZE);
   int i;
   char past = 'a';
   /* for  (i = 0; (str = fgets(str, SIZE, file));) */
   i = 0;
-  while ((str = fgets(str, SIZE, file)))
-    {
-      if (past == str[0])
-	{
-	  printf("Invalid_input: %s\n(invalid 'a' or 'q' at the begginging)\n", str);
-	  exit(1);
-	}
-
-      switch (tolower(str[0])) {
-       case 'q':
-	 data[0][i] = str;
-	 past = 'q';
-	 break;
-       case 'a':
-	 data[1][i++] = str;
-	 past = 'a';
-	 break;
-      case '\n':
-	continue;
-	break;
-      default:
-	printf("Invalid_input: %s\n(could be missing 'a' or 'q)\n", filename);
-	exit(1);
-	break;
-      }
-      str = malloc(sizeof(char) * SIZE);
+  while ((str = fgets(str, SIZE, file))) {
+    if (past == str[0]) {
+      printf("Invalid_input: %s\n(invalid 'a' or 'q' at the begginging)\n",
+             str);
+      exit(1);
     }
+    /* str[strlen(str) - 1] = '\0'; */
+    switch (tolower(str[0])) {
+    case 'q':
+      data[0][i] = str;
+      past = 'q';
+      break;
+    case 'a':
+      data[1][i++] = str;
+      past = 'a';
+      break;
+    case '\n':
+      continue;
+      break;
+    default:
+      printf("Invalid_input: %s\n(could be missing 'a' or 'q)\n", filename);
+      exit(1);
+      break;
+    }
+    str = malloc(sizeof(char) * SIZE);
+  }
   *returnSize = 2;
   *questionsCount = i;
   return data;
 }
 
-{
 void printQuestions(char ***questions, int size) {
   printf("#### Answer Key: ####\n");
   for (int i = 0; i < size; i++)
